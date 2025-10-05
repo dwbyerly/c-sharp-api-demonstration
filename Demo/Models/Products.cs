12c2ba518f5b11a6ef589c21a68a7c147d489ce0
dwbyerly <149709314+dwbyerly@users.noreply.github.com>
2025-10-05 13:34:01 -0400

    create product model

---
+++ Demo/Models/Products.cs
@@ -0,0 +1,15 @@
+using Microsoft.EntityFrameworkCore;
+
+public class Product
+{
+    public int Id { get; set; }
+    public string? Name { get; set; }
+    public decimal Price { get; set; }
+}
+
+public class AppDbContext : DbContext
+{
+    public AppDbContext(DbContextOptions<AppDbContext> options) : base(options){ }
+
+    public DbSet<Product> Products { get; set; }
+}
