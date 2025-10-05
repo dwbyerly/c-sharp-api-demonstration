fb1e4fab825480da94a88281f8ede5f0b5772549
dwbyerly <149709314+dwbyerly@users.noreply.github.com>
2025-10-05 13:47:56 -0400

    basic main program setup

--- Demo/Program.cs
+++ Demo/Program.cs
@@ -1,6 +1,13 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddDbContext<AppDbContext>(
+    options => options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"))
+);
+
+builder.Services.AddControllers();
 var app = builder.Build();
-
-app.MapGet("/", () => "Hello World!");
-
+app.MapControllers();
 app.Run();
